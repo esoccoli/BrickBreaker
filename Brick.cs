61343e38933a685f04d404c72934c56ba3481f91
Ella Soccoli <soccoli2015@gmail.com>
2023-04-03 22:49:34 -0400

    Clear files to rewrite and optimize code

--- Brick.cs
+++ Brick.cs
@@ -7,42 +7,6 @@ namespace BrickBreaker
     /// </summary>
     public class Brick
     {
-        /// <summary>
-        /// Color of the brick
-        /// </summary>
-        private Color color;
         
-        /// <summary>
-        /// Bounds of the brick, which is also used as the hitbox for collision detection
-        /// </summary>
-        public Rectangle Hitbox { get; private set; }
-
-        /// <summary>
-        /// Whether the brick is broken or not
-        /// </summary>
-        public bool Broken { get; set; }
-
-        /// <summary>
-        /// Sets up a brick with a specified hitbox and color
-        /// </summary>
-        /// <param name="hitbox">The hitbox (position) of the brick</param>
-        /// <param name="color">The color of the brick</param>
-        public Brick(Rectangle hitbox, Color color)
-        {
-            Hitbox = hitbox;
-            this.color = color;
-            Broken = false;
-        }
-
-        /// <summary>
-        /// Sets up a brick with a specified hitbox and default color
-        /// </summary>
-        /// <param name="hitbox">The hitbox (position) of the brick</param>
-        public Brick(Rectangle hitbox)
-        {
-            Hitbox = hitbox;
-            color = new Color(217, 15, 42);
-            Broken = false;
-        }
     }
 }
