ae656ee8344027957cd5d1cf4c0f6fba4f8f0a00
Ella Soccoli <soccoli2015@gmail.com>
2023-04-21 23:38:59 -0400

    Fixed menu text positions, attempt 9

--- Menu.cs
+++ Menu.cs
@@ -118,12 +118,12 @@ namespace BrickBreaker
             
             SB.DrawString(PaytoneOneCabinet, "Brick Breaker", new Vector2(Window.Center.X - 300, Window.Center.Y - 150), Color.White);
             
-            SB.DrawString(NotoSansCabinet, "Press ", new Vector2(Window.Center.X - 300, Window.Center.Y), Color.White);
-            SB.Draw(RedButton, new Rectangle(Window.Center.X - 100, Window.Center.Y, 100, 100), Color.White);
+            SB.DrawString(NotoSansCabinet, "Press ", new Vector2(Window.Center.X - 225, Window.Center.Y), Color.White);
+            SB.Draw(RedButton, new Rectangle(Window.Center.X - 75, Window.Center.Y, 100, 100), Color.White);
             SB.DrawString(NotoSansCabinet, " to Start", new Vector2(Window.Center.X + 50 , Window.Center.Y), Color.White);
             
-            SB.DrawString(NotoSansCabinet, "Press ", new Vector2(Window.Center.X - 400, Window.Center.Y + 150), Color.White);
-            SB.Draw(BlueButton, new Rectangle(Window.Center.X - 225, Window.Center.Y + 150, 100, 100), Color.White);
+            SB.DrawString(NotoSansCabinet, "Press ", new Vector2(Window.Center.X - 350, Window.Center.Y + 150), Color.White);
+            SB.Draw(BlueButton, new Rectangle(Window.Center.X - 200, Window.Center.Y + 150, 100, 100), Color.White);
             SB.DrawString(NotoSansCabinet, " for Instructions", new Vector2(Window.Center.X - 75, Window.Center.Y + 150), Color.White);
             
             #endif
