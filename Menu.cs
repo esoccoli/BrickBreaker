501226103fc0c1205ca25797b40b78ac739904ac
Ella Soccoli <soccoli2015@gmail.com>
2023-04-21 23:58:46 -0400

    Fixed menu text positions, attempt 14

--- Menu.cs
+++ Menu.cs
@@ -121,11 +121,11 @@ namespace BrickBreaker
                 "Brick Breaker", 
                 new Vector2(
                     Window.Center.X - PaytoneOne.MeasureString("Brick Breaker").X / 2, 
-                    Window.Center.Y - PaytoneOne.MeasureString("Brick Breaker").Y / 2), 
+                    Window.Center.Y - 150), 
                 Color.White);
             
             SB.DrawString(NotoSansCabinet, "Press ", new Vector2(Window.Center.X - 225, Window.Center.Y), Color.White);
-            SB.Draw(RedButton, new Rectangle(Window.Center.X - 50, Window.Center.Y, 100, 100), Color.White);
+            SB.Draw(RedButton, new Rectangle(Window.Center.X - 60, Window.Center.Y, 100, 100), Color.White);
             SB.DrawString(NotoSansCabinet, " to Start", new Vector2(Window.Center.X + 50 , Window.Center.Y), Color.White);
             
             SB.DrawString(NotoSansCabinet, "Press ", new Vector2(Window.Center.X - 350, Window.Center.Y + 150), Color.White);
